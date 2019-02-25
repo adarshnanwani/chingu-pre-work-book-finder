e12febf74aab6d9eb9a1091042fae862ebd9164e
Adarsh Nanwani <adarsh@pratian.com>
2019-02-25 11:22:44 +0530

    added parent component;

---
+++ src/containers/BookFinder.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import Aux from '../hoc/Auxiliary/Auxiliary';
+
+class BookFinder extends Component {
+  render(){
+    return <Aux/>;
+  }
+}
+
+export default BookFinder;
