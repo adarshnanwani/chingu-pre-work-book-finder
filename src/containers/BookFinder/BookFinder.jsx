42a4604d5c405ad4187013b077e256538010c273
Adarsh Nanwani <adarsh@pratian.com>
2019-02-25 19:41:26 +0530

    Created object for search result

--- src/containers/BookFinder/BookFinder.jsx
+++ src/containers/BookFinder/BookFinder.jsx
@@ -8,22 +8,44 @@ class BookFinder extends Component {
   state = {
     isLoading: false,
     bookResults: [],
-    error:false,
-    searchQuery:''
-  }
+    error: false,
+    searchQuery: ''
+  };
 
   searchBook = () => {
-    axios.get(`volumes?q=${this.state.searchQuery}`).then(response=>{
-      console.log(response);
+    axios.get(`volumes?q=${this.state.searchQuery}`).then(response => {
+      const bookData = response.data.items
+        .map(book => {
+          const bookInfo = { ...book.volumeInfo };
+          const url = bookInfo.infoLink;
+          const authors = bookInfo.authors.reduce((authors, author, index) => {
+            return index === 0
+              ? authors + author
+              : authors + ', ' + author;
+          }, '')
+          const imgLink = bookInfo.imageLinks.thumbnail;
+          const publisher = bookInfo.publisher;
+          const title = bookInfo.title;
+          return {
+            bookUrl: url,
+            authors: authors,
+            imageUrl: imgLink,
+            publisher: publisher,
+            title: title
+          };
+        });
+      this.setState({
+        bookResults: bookData
+      });
     });
   }
 
-  setSearchQuery = (e) =>{
+  setSearchQuery = (e) => {
     const value = e.currentTarget.value;
     this.setState({
-      searchQuery:value
+      searchQuery: value
     });
-  }
+  };
 
   render() {
     return (
@@ -31,7 +53,7 @@ class BookFinder extends Component {
         <SearchArea
           onClick={() => this.searchBook()}
           value={this.state.searchQuery}
-          onChange={(e)=> this.setSearchQuery(e)}
+          onChange={(e) => this.setSearchQuery(e)}
         />
         <ResultsArea />
       </Aux>
