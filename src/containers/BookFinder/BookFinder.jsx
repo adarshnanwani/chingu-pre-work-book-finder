6d3233b9696ef6c3b7dfb6de609b95741d6f42aa
Adarsh Nanwani <adarsh@pratian.com>
2019-02-25 11:40:33 +0530

    removed unused files and updated folders

--- src/containers/BookFinder/BookFinder.jsx
+++ src/containers/BookFinder/BookFinder.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import Aux from '../hoc/Auxiliary/Auxiliary';
+import Aux from '../../hoc/Auxiliary/Auxiliary';
 
 class BookFinder extends Component {
   render(){
