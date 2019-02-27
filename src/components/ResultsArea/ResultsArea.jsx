c755167f37719fc95d6f3420821462117b6a7477
Adarsh Nanwani <adarsh@pratian.com>
2019-02-27 18:57:20 +0530

    Added message for search area

--- src/components/ResultsArea/ResultsArea.jsx
+++ src/components/ResultsArea/ResultsArea.jsx
@@ -3,22 +3,25 @@ import BookRow from './BookRow/BookRow';
 import './ResultsArea.css';
 
 const resultArea = (props) => {
-  const books = props.booksData.reduce((result, book, index) => {
-    if (index % 2 === 0) {
-      let arr = [];
-      arr.push(book);
-      result.push(arr);
-    }
-    else {
-      result[result.length - 1].push(book);
-    }
-    return result;
-  }, [])
-    .map(bookRow => {
-      return <BookRow bookRow={bookRow} key={bookRow[0].bookUrl} seeBookDetails={props.seeBookDetails} />
-    });
-
-
+  let books = (<div className="beforeSearchMessage">
+    <p>Nothing Here Yet - Try Searching For A Book!</p>
+  </div>);
+  if (props.booksData.length > 0) {
+    books = props.booksData.reduce((result, book, index) => {
+      if (index % 2 === 0) {
+        let arr = [];
+        arr.push(book);
+        result.push(arr);
+      }
+      else {
+        result[result.length - 1].push(book);
+      }
+      return result;
+    }, [])
+      .map(bookRow => {
+        return <BookRow bookRow={bookRow} key={bookRow[0].bookUrl} seeBookDetails={props.seeBookDetails} />
+      });
+  }
 
   return (
     <div className="resultsContainer">
